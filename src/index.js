73c7c135ab863058da24ad11ad1fc7965897c3a9
Nikita Ahuja <nahuja038@gmail.com>
2017-12-04 11:28:41 -0600

    Make it work

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new component. This component should produce some HTML.
+
+const App = =>() {
+  return <div>Hi!</div>;
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+// We need to make an instance of app
+// <App></App>
+// <App />
